6e6b70ab64ef8229b701205a1c5970d335eabc78
August <none>
2026-04-01 08:25:09 -0300

    Questão:  We're going to start with a small bit of HDL to get familiar with the interface used by HDLBits. Here's the description of the circuit you need to build for this exercise:
    Build a circuit with no inputs and one output. That output should always drive 1 (or logic high).

---
+++ getting started/getting_started.v
@@ -0,0 +1,17 @@
+/*
+Problem Statement
+
+We're going to start with a small bit of HDL to get familiar with the interface used by HDLBits. Here's the description of the circuit you need to build for this exercise:
+
+Build a circuit with no inputs and one output. That output should always drive 1 (or logic high). 
+*/
+
+
+module getting_started(
+    output one
+);
+
+    assign one = 1'b1; 
+
+
+endmodule 
